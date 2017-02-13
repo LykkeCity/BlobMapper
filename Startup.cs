8b6b806841a7b7ceff3bcbb57a1fec2c70704b78
andreymigin <amigin@gmail.com>
2017-02-13 19:15:29 +0200

    Case insensiteves

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -29,6 +31,48 @@ namespace MvcApp
 
         }
 
+        private static async Task ReadDataFromBlobAsync(string str, Stream dest)
+        {
+            var blob = await ServiceLocator.BlobStorage.GetAsync(ServiceLocator.BlobContainerName, str);
+            blob.CopyTo(dest);
+        }
+
+        private static async Task ReadWithAttempts(string filename, HttpResponse response)
+        {
+
+            var ext = Path.GetExtension(filename);
+
+            if (!string.IsNullOrEmpty(ext)){
+               await ReadDataFromBlobAsync(filename, response.Body);
+                return;
+            }
+
+
+            try
+            {
+                await ReadDataFromBlobAsync(filename, response.Body);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            try
+            {
+                await ReadDataFromBlobAsync(filename+".txt", response.Body);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            await ReadDataFromBlobAsync(filename+".json", response.Body);
+
+        }
+
+
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
@@ -50,10 +94,11 @@ namespace MvcApp
 
                 try
                 {
-                    var str =ctx.Request.Path.Value;
-                    str = str.Substring(1, str.Length - 1);
-                    var blob = await ServiceLocator.BlobStorage.GetAsync(ServiceLocator.BlobContainerName, str);
-                    blob.CopyTo(ctx.Response.Body);
+                    var filename =ctx.Request.Path.Value;
+                    filename = filename.Substring(1, filename.Length - 1).ToLower();
+
+                    await ReadWithAttempts(filename, ctx.Response);
+
                 }
                 catch (Exception)
                 {
